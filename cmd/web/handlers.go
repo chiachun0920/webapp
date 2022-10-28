049a9d285e22ad98d1de32ef8bda64fe8bbff944
Chris Chang <chiachun0920@gmail.com>
2022-10-28 02:11:32 +0000

    Try out form validation at login handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -45,6 +45,13 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	form := NewForm(r.Form)
+	form.Required("email", "password")
+	if !form.Valid() {
+		fmt.Fprintf(w, "failed validation")
+		return
+	}
+
 	email := r.Form.Get("email")
 	password := r.Form.Get("password")
 
