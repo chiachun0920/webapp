3e14ca9e25984a164f73cfe065fd0f3bb7cc7c4d
Chris Chang <chiachun0920@gmail.com>
2022-10-30 16:01:47 +0000

    Create auth middleware

--- cmd/web/middlewares.go
+++ cmd/web/middlewares.go
@@ -34,6 +34,17 @@ func (app *application) addIPToContext(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.Session.Exists(r.Context(), "user") {
+			app.Session.Put(r.Context(), "error", "Please login first")
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func getIP(r *http.Request) (string, error) {
 	ip, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
